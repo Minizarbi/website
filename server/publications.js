6641956274c014e24479f0a509b916acafaa5b12
Minizarbi <minizarbi@hotmail.fr>
2015-12-23 23:56:45 +0100

    Fixed merge conflict

--- server/publications.js
+++ server/publications.js
@@ -10,8 +10,3 @@ Meteor.publish('achievements', function() {
 Meteor.publish('members_achievements', function() {
 	return MembersAchievements.find();
 });
-=======
-Meteor.publish('achievements', function() {
-	return Achievements.find();
-});
->>>>>>> 1c6854217e0518b23357933d0db918ee454e9a0c
